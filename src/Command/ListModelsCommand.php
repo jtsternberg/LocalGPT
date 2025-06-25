ba1d33841e668510cd18b90ec8c013ed6823daf4
Justin Sternberg <justin@dsgnwrks.pro>
2025-06-25 14:31:27 -0400

    Make models command have a verbose flag (todo, use it), and add model/provider filtering.

--- src/Command/ListModelsCommand.php
+++ src/Command/ListModelsCommand.php
@@ -8,6 +8,8 @@ use LocalGPT\Service\ProviderFactory;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
@@ -17,19 +19,50 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 )]
 class ListModelsCommand extends Command
 {
-	public function __construct(protected ?ProviderFactory $providerFactory = null, protected ?ConfigService $configService = null)
+	use VerboseCommandTrait;
+
+	public function __construct(
+		protected ?ProviderFactory $providerFactory = null,
+		protected ?ConfigService $configService = null,
+		bool $verbose = false
+	)
 	{
 		parent::__construct();
+		$this->verbose         = $verbose;
 		$this->configService   = $this->configService ?? new ConfigService();
 		$this->providerFactory = $this->providerFactory ?? new ProviderFactory($this->configService);
+	}
+
+	protected function configure()
+	{
+		$this->addArgument('modelId', InputArgument::OPTIONAL, 'Show details for a specific model ID.');
+		$this->addOption('provider', 'p', InputOption::VALUE_REQUIRED, 'Filter models by provider.');
+		// Verbose seems to already be registered by Symfony, so we don't need to add it here.
+		// $this->addOption('verbose', 'v', InputOption::VALUE_NONE, 'Verbose output.');
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output): int
 	{
 		$io = new SymfonyStyle($input, $output);
-		$io->title('Available Models');
+		$this->setVerbose($input);
 
+		$modelId = $input->getArgument('modelId');
+
+		if ($modelId) {
+			// TODO: Implement model details display.
+			// See ROADMAP.md for more details.
+			$io->writeln("Showing details for model: {$modelId}");
+
+			return Command::SUCCESS;
+		}
+
+
+		$providerFilter = $input->getOption('provider');
 		foreach (array_keys(ProviderFactory::SUPPORTED_PROVIDERS) as $providerName) {
+			if ($providerFilter && $providerName !== $providerFilter) {
+				continue;
+			}
+
 			try {
 				$providerInstance = $this->providerFactory->createProviderByName($providerName);
 				$models           = $providerInstance->listModels();
@@ -40,7 +73,14 @@ class ListModelsCommand extends Command
 				}
 
 				$io->section(ucfirst($providerName) . ' Models');
-				$io->listing($models);
+				if ($this->verbose) {
+					foreach ($models as $model) {
+						// TODO: Display model details.
+						// See ROADMAP.md for more details.
+					}
+				} else {
+					$io->listing($models);
+				}
 			} catch (Exceptions\MissingProviderApiKeyException $e) {
 				// Ignore these.
 			} catch (\Exception $e) {
